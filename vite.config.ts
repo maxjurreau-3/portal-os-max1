1824c078360a390247de9e6d09c8c82c75816863
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-02 17:49:26 -0400

    Modify Vite config to set root and publicDir
    Updated Vite configuration to set root and public directory.

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  },
+  root: ".",           // <— IMPORTANT
+  publicDir: "public", // <— IMPORTANT
   build: {
     outDir: "dist"
   }
