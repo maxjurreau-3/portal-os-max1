15ea4195fc73767b1fd1458d6ad574d19f1e293b
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-03 17:17:09 -0400

    Add main entry point for React application

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// src/main.tsx
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AppV6 } from "../web/AppV6";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppV6 />
+  </React.StrictMode>
+);
