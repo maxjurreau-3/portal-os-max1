a94742197b99a547a4b2c5f2546d3fb891a020b4
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-03 11:28:28 -0400

    Refactor main.tsx to use AppV4 and createRoot

--- web/main.tsx
+++ web/main.tsx
@@ -1,10 +1,14 @@
 // web/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { AppV4 } from "./AppV4";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container #root not found in index.html");
+}
+
+const root = createRoot(container);
+root.render(<AppV4 />);
