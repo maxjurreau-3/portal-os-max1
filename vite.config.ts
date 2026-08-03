4577fda035308b25ffac0d4d47668665d434ba86
maxjurreau-3 <maxjurreau@gmail.com>
2026-08-03 17:46:06 -0400

    Remove build configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: ".",            // IMPORTANT
-  publicDir: "public",  // IMPORTANT
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  }
+  root: "./",
 });
